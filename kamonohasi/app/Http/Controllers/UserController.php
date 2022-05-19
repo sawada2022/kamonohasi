b04545dcad0f50248e3629110a7374bc602c3bba
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-19 17:19:04 +0900

    add index

--- kamonohasi/app/Http/Controllers/UserController.php
+++ kamonohasi/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class UserController extends Controller
 {
     /**
@@ -13,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('index');
+        return view('users/index');
     }
 
     /**
---
+++ kamonohasi/resources/views/books/edit.blade.php
@@ -0,0 +1,9 @@
+<!--@extends('layouts.app')-->
+
+<!--@section('content') -->
+<button>戻る</button>
+
+<h3>蔵書情報</h3>
+@include('commons.book_form')
+
+<!--@endsection-->
