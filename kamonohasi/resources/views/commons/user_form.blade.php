e429e979e9980fdee3cd0c33a8f368296a1f9601
sawada2022 <2022php42-student028@la-study.com>
2022-05-19 15:28:40 +0900

    add user_form

---
+++ kamonohasi/resources/views/commons/user_form.blade.php
@@ -0,0 +1,36 @@
+<form action="{{ route('') }}" method="post">
+    @csrf
+    <label>
+        会員ID
+        <input type="text" name="user_id" value="{{ old('') }}">
+    </label>
+    <label>
+        名前
+        <input type="text" name="user_name" value="{{ old('') }}">
+    </label>
+    <label>
+        メールアドレス
+        <input type="email" name="mail_adress" value="{{ old('') }}">
+    </label>
+    <label>
+        電話番号
+        <input type="tel" name="tel" value="{{ old('') }}">
+    </label>
+    <label>
+        郵便番号
+        <input type="text" name="postal_code" value="{{ old('') }}">
+    </label>
+    <label>
+        住所
+        <input type="text" name="adress" value="{{ old('') }}">
+    </label>
+    <label>
+        生年月日
+        <input type="date" name="birthday" value="{{ old('') }}">
+    </label>
+    <label>
+        備考
+        <input type="text" name="remarks" value="{{ old('') }}">
+    </label>
+    <button>登録</button>
+</form>
