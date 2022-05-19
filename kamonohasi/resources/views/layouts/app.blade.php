6afb20dc5b57ebc243eb2042267ac866e3dc827d
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-19 13:40:28 +0900

    create head

---
+++ kamonohasi/resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>かものはしLibrary<title>
+            <link rel="stylesheet" href="/css/main.css"><!--CSSファイルつくる-->
+    </head>
+    <body>
+        <!--ここに入れる-->
+    </body>
+</html>
