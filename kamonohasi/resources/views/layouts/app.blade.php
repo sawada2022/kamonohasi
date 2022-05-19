b7a14a31d5b571f2e411b7dc9072f2f2b15597dc
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-19 13:59:13 +0900

    create app.php

--- kamonohasi/resources/views/layouts/app.blade.php
+++ kamonohasi/resources/views/layouts/app.blade.php
@@ -3,9 +3,19 @@
     <head>
         <meta charset="utf-8">
         <title>かものはしLibrary<title>
-            <link rel="stylesheet" href="/css/main.css"><!--CSSファイルつくる-->
+         <!--   <link rel="stylesheet" href="/css/main.css">CSSファイルつくる-->
     </head>
     <body>
-        <!--ここに入れる-->
+        <header>
+            <div class="container">
+                <a class="brand" href="/">かものはしLibrary</a>
+                <?php
+                    $filePath = '/kamonohasi.png';
+                    $data = file_get_contents($filePath);// パスから画像データを取得
+                    header('Content-type: image/jpg');// header関数でコンテンツの形式が画像であると宣言
+                    echo $data;//データを出力
+                ?>
+            </div>
+        </header>
     </body>
 </html>
