847c3a7b8fd83800b0acd9240949b9b700373943
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-19 10:21:57 +0900

    add rentals table

---
+++ kamonohasi/database/migrations/2022_05_19_004428_create_rentals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->references('id')->on('users');
+            $table->integer('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rentals');
+    }
+}
