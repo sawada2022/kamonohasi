c271892f8cdae3d218ea0b1675629829646ef905
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-19 10:26:44 +0900

    add information for user model and migrationfile

--- kamonohasi/app/Models/User.php
+++ kamonohasi/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function rental_books(){
+        return $this->belongsToMany(Bok::class, 'rentals');
+    }
+
 }
