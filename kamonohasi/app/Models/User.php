7715862c8cd5e45789d5434432f82f464c5c5102
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-19 10:28:20 +0900

    add information for user table

--- kamonohasi/app/Models/User.php
+++ kamonohasi/app/Models/User.php
@@ -41,9 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    
+
     public function rental_books(){
-        return $this->belongsToMany(Bok::class, 'rentals');
+        return $this->belongsToMany(Book::class, 'rentals');
     }
 
 }
