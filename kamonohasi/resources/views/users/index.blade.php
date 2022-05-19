69f8fa49b419a5c9f331ebcb59a87c6f5ae3ef78
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-19 17:19:34 +0900

    add index

---
+++ kamonohasi/resources/views/users/index.blade.php
@@ -0,0 +1,17 @@
+<!--@extends('layouts.app')-->
+
+<!--@section('content') -->
+<button>戻る</button>
+
+<h3>会員詳細情報</h3>
+<p><a href="{{route('users/create')}}">新規作成</a></p>
+
+<form action="{{route('index')}}" method="post">
+    @csrf
+    <p>
+        <label>会員情報</label>
+        <input type="text" name="user_id">
+        <input type="submit" value="検索">
+    </p>
+</form>
+<!--@endsection-->
