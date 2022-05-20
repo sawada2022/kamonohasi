c270ceebd6687114d08f17f1602d557bf0cd0d94
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-20 09:22:33 +0900

    fixed index

--- kamonohasi/resources/views/users/index.blade.php
+++ kamonohasi/resources/views/users/index.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 
 <h3>会員詳細情報</h3>
-<a href="{{route(user.create)}}">新規登録</a>
+<a href="{{route('users.create')}}">新規登録</a>
 
-<form action="{{route('user.index')}}" method="post">
+<form action="{{route('users.index')}}" method="post">
     <input type="text" name="name" >
     <input type="submit" value="検索">
 </form>
