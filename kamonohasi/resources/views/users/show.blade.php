3ad772b3947dd1112732364c3243f2acd7f615a8
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-20 09:39:03 +0900

    add member&book table

--- kamonohasi/resources/views/users/show.blade.php
+++ kamonohasi/resources/views/users/show.blade.php
@@ -15,12 +15,50 @@
 </form>
 
 <p>会員情報</p>
-<!-- ここに会員情報を表示 -->
+<table>
+    <tbody>
+        <tr>
+            <td>会員ID</td>
+            <td>123456789</td>
+        </tr>
+        <tr>
+            <td>名前</td>
+            <td>山田太郎</td>
+        </tr>
+        <tr>
+            <td>メールアドレス</td>
+            <td>sample@sample.com</td>
+        </tr>
+        <tr>
+            <td>住所</td>
+            <td>東京都○○○○1丁目○○○○</td>
+        </tr>
+    </tbody>
+</table>
 
 <button>編集</button>
 <button>削除</button>
 
 <p>借りている本</p>
-<!-- ここに会員情報を表示 -->
+<table>
+    <thead>
+        <tr>
+            <th>冊数</th>
+            <th>資料名</th>
+            <th>著者</th>
+            <th>出版社</th>
+            <th>貸出期限</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>phpの本</td>
+            <td>山田太郎</td>
+            <td>テスト出版</td>
+            <td>2022年6月1日</td>
+        </tr>
+    </tbody>
+</table>
 
 @endsection
