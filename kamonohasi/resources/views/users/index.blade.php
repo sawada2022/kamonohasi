7c1cb40c87df81665e137f0ff507a894cc8fe4e6
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-20 09:14:27 +0900

    add indexfile

--- kamonohasi/resources/views/users/index.blade.php
+++ kamonohasi/resources/views/users/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 @section('content')
 
-<p>index</p>
+<h3>会員詳細情報</h3>
+<a href="{{route(user.create)}}">新規登録</a>
+
+<form action="{{route('user.index')}}" method="post">
+    <input type="text" name="name" >
+    <input type="submit" value="検索">
+</form>
 
 @endsection
