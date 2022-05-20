ba3c1e14f97a98ec552d470dd6168ca2a5cb6558
sawada2022 <2022php42-student028@la-study.com>
2022-05-20 09:51:59 +0900

    user_edit change

--- kamonohasi/app/Http/Controllers/UserController.php
+++ kamonohasi/app/Http/Controllers/UserController.php
@@ -56,10 +56,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(User $user)
+    public function edit()
     {
         //「http://localhost:8000/users/2/edit」でアクセスすると表示できた！
-        $user->update($request->all());
+        //$user->update($request->all());
         return view('users/edit');
     }
 
--- kamonohasi/resources/views/users/edit.blade.php
+++ kamonohasi/resources/views/users/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h1>会員情報編集</h1>
 
-<form action="{{ route('users.update', $user->id) }}" method="post">
+<form action="{{ route('users.update') }}" method="post">
     @method('patch')
     @include('commons/user_form')
     <button type="submit">更新</button>
