396cd6bb4478905d3d451443f467b4b06ce67565
nakashima-R <105625023+nakashima-R@users.noreply.github.com>
2022-05-20 10:19:28 +0900

    usercreeate

--- kamonohasi/resources/views/users/create.blade.php
+++ kamonohasi/resources/views/users/create.blade.php
@@ -1,6 +1,47 @@
 @extends('layouts.app')
 @section('content')
 
-<p>create</p>
+<button>戻る</button>
+
+<h1>新規会員登録</h1>
+
+<form action="" method="get">
+    @csrf
+
+    <p>
+        <label>会員ID</label>
+        <input type="" name="" value="">
+    </p>
+    <p>
+        <label>名前</label>
+        <input type="text" name="name" value="">
+    </p>
+    <p>
+        <label>メールアドレス</label>
+        <input type="email" name="email" value="">
+    </p>
+    <p>
+        <label>電話番号</label>
+        <input type="" name="" value="">
+    </p>
+    <p>
+        <label>住所</label>
+        <input type="" name="" value="">
+    </p>
+    <p>
+        <label>生年月日</label>
+        <input type="" name="" value="">
+    </p>
+    <p>
+        <label>備考</label>
+        <input type="" name="" value="">
+    </p>
+
+    <button>登録</button>
+</form>
+
+
+
+
 
 @endsection
