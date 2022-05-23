3ae0ec3c77eed7e0387be681c0c48cdbdc7aa6aa
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-23 14:48:44 +0900

    add rentals/index

---
+++ kamonohasi/resources/views/rentals/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+
+<button>戻る</button>
+
+<form action="{{ route('rentals.index', $users->email) }}" method="get">
+    @csrf
+    <label>
+        会員ID
+        <input type="text" name="user_id" value="">
+    </label>
+    <button>検索</button>
+</form>
+
+@if ($flag === 0)
+<table>
+    <tbody>
+        <tr>
+            <td>会員ID</td>
+            <td>{{ $users->id }}</td>
+        </tr>
+        <tr>
+            <td>名前</td>
+            <td>{{ $users->user_name }}</td>
+        </tr>
+    </tbody>
+</table>
+@endif
+
+<a href="{{ route('rentals.create', $users->id) }}">貸出</a>
+<a href="{{ route('rentals.edit', $users->id) }}">返却</a>
+
+@endsection
