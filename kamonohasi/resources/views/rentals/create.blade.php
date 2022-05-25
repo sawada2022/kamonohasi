304c057352fc9ce5102dea245ac77a48cb115461
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-25 12:04:33 +0900

    modal table design 暫定

--- kamonohasi/resources/views/rentals/create.blade.php
+++ kamonohasi/resources/views/rentals/create.blade.php
@@ -5,7 +5,7 @@
 
 <div id="userModal" class="modal">
     <div class="modalContent">
-        <table>
+        <table class="modalTable">
             <tr>
                 <th>ID</th>
                 <td>{{$users->id}}</td>
@@ -29,7 +29,7 @@
 
 <div  id="bookModal" class="modal">
     <div class="modalContent">
-        <table>
+        <table class="modalTable">
             <!-- 対象ユーザが借りている本の表示に変更 -->
             <tr>
                 <th>資料名</th>
