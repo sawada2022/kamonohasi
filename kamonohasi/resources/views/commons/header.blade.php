5fa9c0fc75c23df0b824caeff8e86eb703b6fedc
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-26 09:48:43 +0900

    header add link index

--- kamonohasi/resources/views/commons/header.blade.php
+++ kamonohasi/resources/views/commons/header.blade.php
@@ -1,9 +1,20 @@
 <header>
-    <div>
+    <div class="headerLeft">
         <a href="/">
             <p class="headerText">かものはしLibrary</p>
         </a>
         <img src="/image/kamonohasi.png" alt="かものはしのイラスト" />          
         <p class="headerText">@yield('title')</p>
+    </div>
+    <div class="headerRight">
+        <a href="{{route('rentals.index')}}">
+            <p class="headerTextLink">貸借業務</p>
+        </a>
+        <a href="{{route('users.index')}}">
+            <p class="headerTextLink">会員管理業務</p>
+        </a>
+        <a href="{{route('books.index')}}">
+            <p class="headerTextLink">資料管理業務</p>
+        </a>
     </div>
 </header>
