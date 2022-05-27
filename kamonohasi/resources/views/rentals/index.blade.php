73b809f44b86530ad6d7068930cb73907e1c06a8
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-27 16:49:00 +0900

    rental btns design!!!!

--- kamonohasi/resources/views/rentals/index.blade.php
+++ kamonohasi/resources/views/rentals/index.blade.php
@@ -14,15 +14,16 @@
 </form>
 
 @if ($flag === 0)
-<div class="rentalShowUserFlex rentalIndexResult">
+<div class="rentalIndexResult">
     <h3>会員情報</h3>
     <p>会員ID：{{ $users->id }}</p>
     <p>名前：{{ $users->user_name }}</p>
 </div>
 
-<a href="{{ route('rentals.create', ['user_id' => $users,'book_flag' => $book_flag, 'rental_flag' => $rental_flag, 'rentals' => $rentals]) }}" id="btn1">貸出</a>
-
-<a href="{{ route('rentals.edit', [$users->id,'book_flag' => $book_flag]) }}" id="btn2" >返却</a>
+<div class="rentalIndexBtnArea">
+    <a href="{{ route('rentals.create', ['user_id' => $users,'book_flag' => $book_flag, 'rental_flag' => $rental_flag, 'rentals' => $rentals]) }}" id="rental-index-btn1" class="rentalIndexBtnBase">貸出 <i class="fa-solid fa-plus"></i></a>
+    <a href="{{ route('rentals.edit', [$users->id,'book_flag' => $book_flag]) }}" id="rental-index-btn2" class="rentalIndexBtnBase">返却 <i class="fa-solid fa-rotate-left"></i></a>
+</div>
 
 @endif
 @endsection
