85197cf08101944c2540d0fe267003685b83daef
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-27 13:03:06 +0900

    add old function for book_form

--- kamonohasi/resources/views/commons/book_form.blade.php
+++ kamonohasi/resources/views/commons/book_form.blade.php
@@ -4,43 +4,43 @@
     @csrf
     <label>
         資料名
-        <input type="text" name="title" value="{{ $book->title }}">
+        <input type="text" name="title" value="{{ old('title',$book->title) }}">
     </label>
     <label>
         著者
-        <input type="text" name="author" value="{{ $book->author }}">
+        <input type="text" name="author" value="{{ old('author',$book->author) }}">
     </label>
     <label>
         出版社
-        <input type="text" name="publisher" value="{{ $book->publisher }}">
+        <input type="text" name="publisher" value="{{ old('publisher',$book->publisher) }}">
     </label>
     <label>
         ISBN番号
-        <input type="text" name="isbn" value="{{ $book->isbn }}">
+        <input type="text" name="isbn" value="{{ old('isbn',$book->isbn) }}">
     </label>
     <label for="category">分類コード</label>
     <select id="category" name="category_id">
         @foreach($categories as $category)
         @if($category->category_id === $book->category_id)
-        <option value="{{ $category->category_id }}" selected>
+        <option value="{{ old('category_id',$category->category_id) }}" selected>
         @else
-        <option value="{{ $category->category_id }}">
+        <option value="{{ old('category_id',$category->category_id) }}">
         @endif
-            {{ $category->category_id }}：{{ $category->genre }}
+            {{ old('category_id',$category->category_id) }}：{{ old('genre',$category->genre) }}
         </option>
         @endforeach
     </select>
     <label>
         出版日
-        <input type="date" name="published_at" value="{{ $book->published_on }}">
+        <input type="date" name="published_at" value="{{  old('published_on',$book->published_on )}}">
     </label>
     <label>
         備考
-        <input type="text" name="comment" value="{{ $book->comment }}">
+        <input type="text" name="comment" value="{{ old('comment', $book->comment) }}">
     </label>
     <label>
         廃棄年月日
-        <input type="date" name="deleted_at" value="{{ $book->deleted_on }}">
+        <input type="date" name="deleted_at" value="{{ old('deleted_on',$book->deleted_on) }}">
     </label>
     <button>登録</button>
 </form>
