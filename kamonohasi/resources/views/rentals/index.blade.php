74efed4cc25d8cae79a1ce11282a23eb3c9be48c
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-27 16:31:26 +0900

    add rental index result

--- kamonohasi/resources/views/rentals/index.blade.php
+++ kamonohasi/resources/views/rentals/index.blade.php
@@ -14,19 +14,14 @@
 </form>
 
 @if ($flag === 0)
-<table>
-    <tbody>
-        <tr>
-            <td>会員ID</td>
-            <td>{{ $users->id }}</td>
-        </tr>
-        <tr>
-            <td>名前</td>
-            <td>{{ $users->user_name }}</td>
-        </tr>
-    </tbody>
-</table>
+<div class="rentalShowUserFlex rentalIndexResult">
+    <h3>会員情報</h3>
+    <p>会員ID：{{ $users->id }}</p>
+    <p>名前：{{ $users->user_name }}</p>
+</div>
+
 <a href="{{ route('rentals.create', ['user_id' => $users,'book_flag' => $book_flag, 'rental_flag' => $rental_flag, 'rentals' => $rentals]) }}" id="btn1">貸出</a>
+
 <a href="{{ route('rentals.edit', [$users->id,'book_flag' => $book_flag]) }}" id="btn2" >返却</a>
 
 @endif
