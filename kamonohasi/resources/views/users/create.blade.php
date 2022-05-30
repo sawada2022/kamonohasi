e86ff95ed49fdc13d880e995bc9f6e7dd95a4900
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-30 10:50:43 +0900

    users create design

--- kamonohasi/resources/views/users/create.blade.php
+++ kamonohasi/resources/views/users/create.blade.php
@@ -5,13 +5,15 @@
 
 
 <h1>会員情報登録</h1>
-@include('commons/flash')
-<form action="{{ route('users.store', $user->id) }}" method="post">
-    @csrf
-    @include('commons/user_form')
 
-    <input type="submit" value="登録">
-</form>
+<div class="singleFormStyle">
+    @include('commons/flash')
+    <form action="{{ route('users.store', $user->id) }}" method="post"  class="card singleForm">
+        @csrf
+        @include('commons/user_form')
 
+        <button class="btnBase"><i class="fa-solid fa-plus"></i><span>登録</span></button>
+    </form>
+</div>
 
 @endsection
