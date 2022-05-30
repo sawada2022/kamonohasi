2cdcf780ce1230827aa61d21cb2cce941fb54973
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-30 13:17:43 +0900

    home btn design

--- kamonohasi/resources/views/home.blade.php
+++ kamonohasi/resources/views/home.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 
 
-<a href="{{route('rentals.index')}}">貸借業務</a>
-<a href="{{route('users.index')}}">会員管理業務</a>
-<a href="{{route('books.index')}}">資料管理業務</a>
+<a href="{{route('rentals.index')}}" class="btn btn-malformation top-green">貸借業務</a>
+<a href="{{route('users.index')}}" class="btn btn-malformation top-yellow">会員管理業務</a>
+<a href="{{route('books.index')}}" class="btn btn-malformation top-light-green">資料管理業務</a>
 
 
 @endsection
