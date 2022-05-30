0a54a6e7a17e21d3622fcbeb014b4177fd943cb0
sawada2022 <2022php42-student028@la-study.com>
2022-05-30 14:19:07 +0900

    ok

--- kamonohasi/resources/views/books/create.blade.php
+++ kamonohasi/resources/views/books/create.blade.php
@@ -29,7 +29,7 @@
         <span>
             <label for="category">分類コード</label>
             <select id="category" name="category_id">
-                <!--初期表示しないようにする-->
+                <option value=""></option>
                 @foreach($categories as $category)
                 <option value="{{ $category->category_id }}">
                     {{ $category->category_id }}：{{ $category->genre }}
