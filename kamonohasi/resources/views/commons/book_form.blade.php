fb6af33e3aab533068e4c747378764a87ce41303
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-30 10:16:26 +0900

    books edit design

--- kamonohasi/resources/views/commons/book_form.blade.php
+++ kamonohasi/resources/views/commons/book_form.blade.php
@@ -2,45 +2,49 @@
 <!-- optionをforeachで自動生成する -->
 <!-- old関数の中身を追加 -->
     @csrf
-    <label>
-        資料名
-        <input type="text" name="title" value="{{ old('title',$book->title) }}">
-    </label>
-    <label>
-        著者
-        <input type="text" name="author" value="{{ old('author',$book->author) }}">
-    </label>
-    <label>
-        出版社
-        <input type="text" name="publisher" value="{{ old('publisher',$book->publisher) }}">
-    </label>
-    <label>
-        ISBN番号
-        <input type="text" name="isbn" value="{{ old('isbn',$book->isbn) }}">
-    </label>
-    <label for="category">分類コード</label>
-    <select id="category" name="category_id">
-        @foreach($categories as $category)
-        @if($category->category_id === $book->category_id)
-        <option value="{{ old('category_id',$category->category_id) }}" selected>
-        @else
-        <option value="{{ old('category_id',$category->category_id) }}">
-        @endif
-            {{ old('category_id',$category->category_id) }}：{{ old('genre',$category->genre) }}
-        </option>
-        @endforeach
-    </select>
-    <label>
-        出版日
-        <input type="date" name="published_at" value="{{  old('published_on',$book->published_on )}}">
-    </label>
-    <label>
-        備考
-        <input type="text" name="comment" value="{{ old('comment', $book->comment) }}">
-    </label>
-    <label>
-        廃棄年月日
-        <input type="date" name="deleted_at" value="{{ old('deleted_on',$book->deleted_on) }}">
-    </label>
-    <button>登録</button>
+    <div class="searchFormInputFlex">
+        <span>
+            <label for="title">資料名</label>
+            <input type="text" id="title" name="title" value="{{ old('title',$book->title) }}">
+        </span>
+        <span>
+            <label for="author">著者</label>
+            <input type="text" id="author" name="author" value="{{ old('author',$book->author) }}">
+        </span>
+        <span>
+            <label for="publisher">出版社</label>
+            <input type="text" id="publisher" name="publisher" value="{{ old('publisher',$book->publisher) }}">
+        </span>
+        <span>
+            <label for="isbn">ISBN番号</label>
+            <input type="text" id="isbn" name="isbn" value="{{ old('isbn',$book->isbn) }}">
+        </span>
+        <span>
+            <label for="category">分類コード</label>
+            <select id="category" name="category_id">
+                @foreach($categories as $category)
+                @if($category->category_id === $book->category_id)
+                <option value="{{ old('category_id',$category->category_id) }}" selected>
+                @else
+                <option value="{{ old('category_id',$category->category_id) }}">
+                @endif
+                    {{ old('category_id',$category->category_id) }}：{{ old('genre',$category->genre) }}
+                </option>
+                @endforeach
+            </select>
+        </span>
+        <span>
+            <label for="published_at">出版日</label>
+            <input type="date" id="published_at" name="published_at" value="{{  old('published_on',$book->published_on )}}">
+        </span>
+        <span>
+            <label for="comment">備考</label>
+            <input type="text" id="comment" name="comment" value="{{ old('comment', $book->comment) }}">
+        </span>
+        <span>
+            <label for="deleted_at">廃棄年月日</label>
+            <input type="date" id="deleted_at" name="deleted_at" value="{{ old('deleted_on',$book->deleted_on) }}">
+        </span>
+    </div>
+    <button class="btnBase"><i class="fa-solid fa-check"></i><span>更新</span></button>
 </form>
