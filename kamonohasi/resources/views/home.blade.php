b68eb866a4820100f51045fe54995e1a7942a11e
yanagida-2022php <105625038+yanagida-2022php@users.noreply.github.com>
2022-05-30 13:40:44 +0900

    home design

--- kamonohasi/resources/views/home.blade.php
+++ kamonohasi/resources/views/home.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="homeMainArea">
+    <div class="homeTitle">
+        <p>かものはしLibrary</p>
+        <img src="/image/kamonohasi.png" alt="かものはしのイラスト" /> 
+    </div>
 
-<a href="{{route('rentals.index')}}" class="btn btn-malformation top-green">貸借業務</a>
-<a href="{{route('users.index')}}" class="btn btn-malformation top-yellow">会員管理業務</a>
-<a href="{{route('books.index')}}" class="btn btn-malformation top-light-green">資料管理業務</a>
-
-
+    <div class="homeBtnArea">
+        <a href="{{route('rentals.index')}}" class="btn btn-malformation top-green"><i class="fa-solid fa-book-open-reader"></i><span>貸借業務</span></a>
+        <a href="{{route('users.index')}}" class="btn btn-malformation top-yellow"><i class="fa-solid fa-user"></i><span>会員管理業務</span></a>
+        <a href="{{route('books.index')}}" class="btn btn-malformation top-light-green"><i class="fa-solid fa-book"></i><span>資料管理業務</span></a>
+    </div>
+</div>
 @endsection
