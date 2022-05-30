3ef119b687e43da2f13dd1c4ecd010aa9675f404
sara9942 <105625017+sara9942@users.noreply.github.com>
2022-05-30 11:19:28 +0900

    added create old function

--- kamonohasi/resources/views/books/create.blade.php
+++ kamonohasi/resources/views/books/create.blade.php
@@ -12,19 +12,19 @@
     <div class="searchFormInputFlex">
         <span>
             <label for="title">資料名</label>
-            <input type="text" id="title" name="title" value="{{ $book->title }}">
+            <input type="text" id="title" name="title" value="{{ old('title', $book->title) }}">
         </span>
         <span>
             <label for="author">著者</label>
-            <input type="text" id="author" name="author" value="{{ $book->author }}">
+            <input type="text" id="author" name="author" value="{{ old('author',$book->author) }}">
         </span>
         <span>
             <label for="publisher">出版社</label>
-            <input type="text" id="publisher" name="publisher" value="{{ $book->publisher }}">
+            <input type="text" id="publisher" name="publisher" value="{{ old('publisher',$book->publisher )}}">
         </span>
         <span>
             <label for="isbn">ISBN番号</label>
-            <input type="text" id="isbn" name="isbn" value="{{ $book->isbn }}">
+            <input type="text" id="isbn" name="isbn" value="{{ old('isbn',$book->isbn) }}">
         </span>
         <span>
             <label for="category">分類コード</label>
@@ -39,15 +39,15 @@
         </span>
         <span>
             <label for="published_on">出版日</label>
-            <input type="date" id="published_on" name="published_on" value="{{ $book->published_on }}">
+            <input type="date" id="published_on" name="published_on" value="{{ old('published_on',$book->published_on) }}">
         </span>
         <span>
             <label for="comment">備考</label>
-            <input type="text" id="comment" name="comment" value="{{ $book->comment }}">
+            <input type="text" id="comment" name="comment" value="{{ old('comment',$book->comment) }}">
         </span>
         <span>
             <label for="deleted_on">廃棄年月日</label>
-            <input type="date" id="deleted_on" name="deleted_on" value="{{ $book->deleted_on }}">
+            <input type="date" id="deleted_on" name="deleted_on" value="{{ old('deleted_on',$book->deleted_on) }}">
         </span>
     </div>
     <button class="btnBase"><i class="fa-solid fa-plus"></i><span>登録</span></button>
